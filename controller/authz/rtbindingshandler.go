066224923d103d72de317d0d82b12e65f54a9dd0
kinarashah <kinarahs@usc.edu>
2017-12-17 13:17:50 -0800

    RoleTemplate Controller

--- controller/authz/rtbindingshandler.go
+++ controller/authz/rtbindingshandler.go
@@ -46,6 +46,7 @@ func Register(workload *config.ClusterContext) {
 	}
 	workload.Management.Management.ProjectRoleTemplateBindings("").Controller().AddHandler(r.syncPRTB)
 	workload.Management.Management.ClusterRoleTemplateBindings("").Controller().AddHandler(r.syncCRTB)
+	workload.Management.Management.RoleTemplates("").Controller().AddHandler(r.syncRT)
 	workload.Core.Namespaces("").Controller().AddHandler(r.syncNS)
 }
 
@@ -221,6 +222,65 @@ func (r *roleHandler) ensurePRTBDelete(key string, binding *v3.ProjectRoleTempla
 		_, err := r.workload.Management.Management.ProjectRoleTemplateBindings("").Update(binding)
 		return err
 	}
+	return nil
+}
+
+func (r *roleHandler) syncRT(key string, template *v3.RoleTemplate) error {
+	if template == nil {
+		return nil
+	}
+
+	if template.DeletionTimestamp != nil {
+		return r.ensureRTDelete(key, template)
+	}
+
+	return r.ensureRT(key, template)
+}
+
+func (r *roleHandler) ensureRT(key string, template *v3.RoleTemplate) error {
+	template = template.DeepCopy()
+	if r.addFinalizer(template) {
+		if _, err := r.workload.Management.Management.RoleTemplates("").Update(template); err != nil {
+			return errors.Wrapf(err, "couldn't set finalizer on %v", key)
+		}
+	}
+
+	roles := map[string]*v3.RoleTemplate{}
+	if err := r.gatherRoles(template, roles); err != nil {
+		return err
+	}
+
+	if err := r.ensureRoles(roles); err != nil {
+		return errors.Wrapf(err, "couldn't ensure roles")
+	}
+
+	return nil
+}
+
+func (r *roleHandler) ensureRTDelete(key string, template *v3.RoleTemplate) error {
+	if len(template.ObjectMeta.Finalizers) <= 0 || template.ObjectMeta.Finalizers[0] != finalizerName {
+		return nil
+	}
+
+	template = template.DeepCopy()
+
+	roles := map[string]*v3.RoleTemplate{}
+	if err := r.gatherRoles(template, roles); err != nil {
+		return err
+	}
+
+	roleCli := r.workload.K8sClient.RbacV1().ClusterRoles()
+	for _, role := range roles {
+		if err := roleCli.Delete(role.Name, &metav1.DeleteOptions{}); err != nil {
+			return errors.Wrapf(err, "error deleting clusterrole %v", role.Name)
+		}
+	}
+
+	if r.removeFinalizer(template) {
+		_, err := r.workload.Management.Management.RoleTemplates("").Update(template)
+		return err
+	}
+
 	return nil
 }
 
