4fa5720d8005730bb43275fd526c28c4f0b080db
Daishan Peng <daishan@rancher.com>
2018-01-10 11:19:55 -0700

    add helm controller

--- controller/controllers.go
+++ controller/controllers.go
@@ -9,6 +9,7 @@ import (
 	"github.com/rancher/cluster-agent/controller/nodesyncer"
 	"github.com/rancher/cluster-agent/controller/secret"
 	"github.com/rancher/types/config"
+	helmController "github.com/rancher/helm-controller/controller"
 	workloadController "github.com/rancher/workload-controller/controller"
 )
 
@@ -18,6 +19,7 @@ func Register(ctx context.Context, cluster *config.ClusterContext) error {
 	authz.Register(cluster)
 	eventssyncer.Register(cluster)
 	secret.Register(cluster)
+	helmController.Register(cluster)
 
 	workloadContext := cluster.WorkloadContext()
 	return workloadController.Register(ctx, workloadContext)
