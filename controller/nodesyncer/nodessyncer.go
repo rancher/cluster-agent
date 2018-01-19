ecc787dfd5481245db52c00982cee59017d78b14
Alena Prokharchyk <alena@rancher.com>
2018-01-18 16:38:20 -0800

    Reconcile logic for node syncer

--- controller/nodesyncer/nodessyncer.go
+++ controller/nodesyncer/nodessyncer.go
@@ -2,10 +2,10 @@ package nodesyncer
 
 import (
 	"fmt"
-
 	"reflect"
 
 	"github.com/pkg/errors"
+	"github.com/rancher/types/apis/core/v1"
 	"github.com/rancher/types/apis/management.cattle.io/v3"
 	"github.com/rancher/types/config"
 	"github.com/sirupsen/logrus"
@@ -15,49 +15,178 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 )
 
+const (
+	allMachineKey = "_machine_all_"
+)
+
 type NodeSyncer struct {
 	machinesClient   v3.MachineInterface
+	clusterNamespace string
+}
+
+type MachinesSyncer struct {
+	machinesClient   v3.MachineInterface
 	machines         v3.MachineLister
+	nodes            v1.NodeLister
 	clusters         v3.ClusterLister
-	clusterName      string
 	clusterNamespace string
 }
 
 func Register(cluster *config.ClusterContext) {
 	n := &NodeSyncer{
-		clusterName:      cluster.ClusterName,
+		clusterNamespace: cluster.ClusterName,
+		machinesClient:   cluster.Management.Management.Machines(cluster.ClusterName),
+	}
+
+	m := &MachinesSyncer{
 		clusterNamespace: cluster.ClusterName,
 		machinesClient:   cluster.Management.Management.Machines(cluster.ClusterName),
 		machines:         cluster.Management.Management.Machines(cluster.ClusterName).Controller().Lister(),
 		clusters:         cluster.Management.Management.Clusters("").Controller().Lister(),
+		nodes:            cluster.Core.Nodes("").Controller().Lister(),
 	}
-	cluster.Core.Nodes("").AddLifecycle("nodesSyncer", n)
+	cluster.Core.Nodes("").Controller().AddHandler("nodesSyncer", n.sync)
+	cluster.Management.Management.Machines(cluster.ClusterName).Controller().AddHandler("machinesSyncer", m.sync)
 }
 
-func (n *NodeSyncer) Remove(node *corev1.Node) (*corev1.Node, error) {
-	machine, err := n.getMachine(node.Name)
-	if err != nil {
-		return nil, err
-	}
-	logrus.Infof("Deleting cluster node [%s]", node.Name)
+func (n *NodeSyncer) sync(key string, node *corev1.Node) error {
+	n.machinesClient.Controller().Enqueue(n.clusterNamespace, allMachineKey)
+	return nil
+}
 
+func (m *MachinesSyncer) sync(key string, machine *v3.Machine) error {
+	if key == fmt.Sprintf("%s/%s", m.clusterNamespace, allMachineKey) {
+		return m.reconcileAll()
+	}
+	return nil
+}
+
+func (m *MachinesSyncer) reconcileAll() error {
+	nodes, err := m.nodes.List("", labels.NewSelector())
+	if err != nil {
+		return err
+	}
+
+	nodeMap := make(map[string]*corev1.Node)
+	for _, node := range nodes {
+		nodeMap[node.Name] = node
+	}
+
+	machines, err := m.machines.List(m.clusterNamespace, labels.NewSelector())
+	machineMap := make(map[string]*v3.Machine)
+	for _, machine := range machines {
+		nodeName := getNodeNameFromMachine(machine)
+		if nodeName == "" {
+			logrus.Warnf("Failed to get nodeName from machine [%s]", machine.Name)
+			continue
+		}
+		machineMap[nodeName] = machine
+	}
+
+	// reconcile machines for existing nodes
+	for name, node := range nodeMap {
+		machine, _ := machineMap[name]
+		remove := false
+		if node.DeletionTimestamp != nil {
+			if machine == nil {
+				// machine is already removed
+				continue
+			}
+			remove = true
+		}
+		err = m.reconcileMachineForNode(machine, remove, node)
+		if err != nil {
+			return err
+		}
+	}
+	// run the logic for machine to remove
+	for name, machine := range machineMap {
+		if _, ok := nodeMap[name]; !ok {
+			m.reconcileMachineForNode(machine, true, nil)
+		}
+	}
+
+	return nil
+}
+
+func (m *MachinesSyncer) reconcileMachineForNode(machine *v3.Machine, remove bool, node *corev1.Node) error {
+	if remove {
+		return m.removeMachine(machine)
+	}
 	if machine == nil {
-		logrus.Debugf("Cluster node [%s] is already deleted")
-		return nil, nil
+		return m.createMachine(node)
+	} else {
+		return m.updateMachine(machine, node)
 	}
-	err = n.machinesClient.Delete(machine.ObjectMeta.Name, nil)
-	if err != nil {
-		return nil, errors.Wrapf(err, "Failed to delete cluster node [%s]", node.Name)
-	}
-	logrus.Infof("Deleted cluster node [%s]", node.Name)
-	return nil, nil
 }
 
-func (n *NodeSyncer) getMachine(nodeName string) (*v3.Machine, error) {
-	machines, err := n.machines.List(n.clusterNamespace, labels.NewSelector())
+func (m *MachinesSyncer) removeMachine(machine *v3.Machine) error {
+	err := m.machinesClient.Delete(machine.ObjectMeta.Name, nil)
 	if err != nil {
-		return nil, err
+		return errors.Wrapf(err, "Failed to delete machine [%s]", machine.Name)
 	}
+	logrus.Infof("Deleted cluster node [%s]", machine.Name)
+	return nil
+}
+
+func (m *MachinesSyncer) updateMachine(existing *v3.Machine, node *corev1.Node) error {
+	toUpdate, err := m.convertNodeToMachine(node, existing)
+	if err != nil {
+		return err
+	}
+	// update only when nothing changed
+	if objectsAreEqual(existing, toUpdate) {
+		return nil
+	}
+	logrus.Debugf("Updating machine for node [%s]", node.Name)
+	_, err = m.machinesClient.Update(toUpdate)
+	if err != nil {
+		return errors.Wrapf(err, "Failed to update machine for node [%s]", node.Name)
+	}
+	logrus.Debugf("Updated machine for node [%s]", node.Name)
+	return nil
+}
+
+func (m *MachinesSyncer) createMachine(node *corev1.Node) error {
+	// try to get machine from api, in case cache didn't get the update
+	existing, err := m.getMachine(node.Name, false)
+	if err != nil {
+		return err
+	}
+	if existing != nil {
+		return nil
+	}
+	machine, err := m.convertNodeToMachine(node, existing)
+	if err != nil {
+		return err
+	}
+
+	_, err = m.machinesClient.Create(machine)
+	if err != nil {
+		return errors.Wrapf(err, "Failed to create machine for node [%s]", node.Name)
+	}
+	logrus.Infof("Created machine for node [%s]", node.Name)
+	return nil
+}
+
+func (m *MachinesSyncer) getMachine(nodeName string, cache bool) (*v3.Machine, error) {
+	var machines []*v3.Machine
+	var err error
+	if cache {
+		machines, err = m.machines.List(m.clusterNamespace, labels.NewSelector())
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		machinelist, err := m.machinesClient.List(metav1.ListOptions{})
+		if err != nil {
+			return nil, err
+		}
+		for _, machine := range machinelist.Items {
+			machines = append(machines, &machine)
+		}
+	}
+
 	for _, machine := range machines {
 		if machine.Status.NodeName == nodeName {
 			return machine, nil
@@ -71,6 +200,19 @@ func (n *NodeSyncer) getMachine(nodeName string) (*v3.Machine, error) {
 	}
 
 	return nil, nil
+}
+
+func getNodeNameFromMachine(machine *v3.Machine) string {
+	if machine.Status.NodeName != "" {
+		return machine.Status.NodeName
+	}
+	// to handle the case when machine was provisioned first
+	if machine.Status.NodeConfig != nil {
+		if machine.Status.NodeConfig.HostnameOverride != "" {
+			return machine.Status.NodeConfig.HostnameOverride
+		}
+	}
+	return ""
 }
 
 func resetConditions(machine *v3.Machine) *v3.Machine {
@@ -89,28 +231,6 @@ func resetConditions(machine *v3.Machine) *v3.Machine {
 	return updated
 }
 
-func (n *NodeSyncer) Updated(node *corev1.Node) (*corev1.Node, error) {
-	existing, err := n.getMachine(node.Name)
-	if err != nil || existing == nil {
-		return nil, err
-	}
-	toUpdate, err := n.convertNodeToMachine(node, existing)
-	if err != nil {
-		return nil, err
-	}
-	// update only when nothing changed
-	if objectsAreEqual(existing, toUpdate) {
-		return nil, nil
-	}
-	logrus.Debugf("Updating cluster node [%s]", node.Name)
-	_, err = n.machinesClient.Update(toUpdate)
-	if err != nil {
-		return nil, errors.Wrapf(err, "Failed to update cluster node [%s]", node.Name)
-	}
-	logrus.Debugf("Updated cluster node [%s]", node.Name)
-	return nil, nil
-}
-
 func objectsAreEqual(existing *v3.Machine, toUpdate *v3.Machine) bool {
 	// we are updating spec and status only, so compare them
 	toUpdateToCompare := resetConditions(toUpdate)
@@ -123,15 +243,7 @@ func objectsAreEqual(existing *v3.Machine, toUpdate *v3.Machine) bool {
 	return statusEqual && specEqual && nodeNameEqual && labelsEqual && annotationsEqual
 }
 
-func (n *NodeSyncer) convertNodeToMachine(node *corev1.Node, existing *v3.Machine) (*v3.Machine, error) {
-	cluster, err := n.clusters.Get("", n.clusterName)
-	if err != nil {
-		return nil, errors.Wrapf(err, "Failed to get cluster [%s]", n.clusterName)
-	}
-	if cluster.ObjectMeta.DeletionTimestamp != nil {
-		return nil, fmt.Errorf("Failed to find cluster [%s]", n.clusterName)
-	}
-
+func (m *MachinesSyncer) convertNodeToMachine(node *corev1.Node, existing *v3.Machine) (*v3.Machine, error) {
 	var machine *v3.Machine
 	if existing == nil {
 		machine = &v3.Machine{
@@ -140,7 +252,7 @@ func (n *NodeSyncer) convertNodeToMachine(node *corev1.Node, existing *v3.Machin
 			ObjectMeta: metav1.ObjectMeta{
 				GenerateName: "machine-"},
 		}
-		machine.Namespace = n.clusterNamespace
+		machine.Namespace = m.clusterNamespace
 		machine.Status.Requested = make(map[corev1.ResourceName]resource.Quantity)
 		machine.Status.Limits = make(map[corev1.ResourceName]resource.Quantity)
 		machine.Spec.NodeSpec = *node.Spec.DeepCopy()
@@ -159,23 +271,4 @@ func (n *NodeSyncer) convertNodeToMachine(node *corev1.Node, existing *v3.Machin
 	machine.APIVersion = "management.cattle.io/v3"
 	machine.Kind = "Machine"
 	return machine, nil
-}
-
-func (n *NodeSyncer) Create(node *corev1.Node) (*corev1.Node, error) {
-	existing, err := n.getMachine(node.Name)
-	if err != nil || existing != nil {
-		return nil, err
-	}
-	machine, err := n.convertNodeToMachine(node, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	logrus.Infof("Creating cluster node [%s]", node.Name)
-	_, err = n.machinesClient.Create(machine)
-	if err != nil {
-		return nil, errors.Wrapf(err, "Failed to create cluster node [%s]", node.Name)
-	}
-	logrus.Infof("Created cluster node [%s]", node.Name)
-	return nil, nil
 }
